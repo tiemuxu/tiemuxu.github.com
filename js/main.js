88721fdb3d96300a9c51d72792eb4bf88aebc502
tiemuxu <tiemuxu@gamil.com>
2013-11-09 22:35:10 +0800

    try to fix some bugs for new js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ var navigationbar = new Tiemuxu.NavBar();
 var hiddenbar = new Tiemuxu.HiddenBar();
 var blogpage = new Tiemuxu.Page();
 var widgets = new Tiemuxu.widgets();
+
+navigationbar.init();
+hiddenbar.init();
+blogpage.init();
+widgets = init();
